2aa125cd890fa9634c57456b01ce9c0063a4169b
rgarant <rgarant@tenable.com>
2023-07-11 14:26:16 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('oh hello.')
+  }, 3000)
+}).catch(() => {})
+
+function use(promise) {
+  if (promise.status === 'fulfilled') {
+    return promise.value
+  } else if (promise.status === 'rejected') {
+    console.log('reason', promise.reason, typeof promise.reason)
+    throw new Error(promise.reason)
+  } else if (promise.status === 'pending') {
+    throw promise
+  } else {
+    promise.status = 'pending'
+    promise.then(
+      (result) => {
+        promise.status = 'fulfilled'
+        promise.value = result
+      },
+      (reason) => {
+        promise.status = 'rejected'
+        promise.reason = reason
+      }
+    )
+    throw promise
+  }
+}
+
+function SusComponent() {
+  console.log('rendering')
+
+  const data = use(promise)
+  console.log('callb')
+
+  return <div>Ready: {data}</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SusComponent className="App" />
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
-export default App;
+export default App
